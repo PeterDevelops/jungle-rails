55f6ce019f80f34539031dfacc62063178ade7d3
PeterDevelops <PeterDinhBC@gmail.com>
2024-07-08 13:16:31 -0700

    update Product and Category model

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,14 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
 
   has_many :products
-
+  has_many :line_items, dependent: :destroy
 end
